789ef51f6d6e7d8695c731090ae943a8fbdd3dec
milan.tomin <milan.tomin@vega.local>
2022-10-20 14:30:20 +0200

    commit

--- cinema-app/src/Components/ticket/ticket.js
+++ cinema-app/src/Components/ticket/ticket.js
@@ -6,6 +6,7 @@ export default function Ticket(prop){
     const k = prop.type;
     const logo = require('../../Assets/img1.jpg')
 
+    if(k === 0){
     return (
 
         <div className={classes.wrapper}>
@@ -23,5 +24,24 @@ export default function Ticket(prop){
             </div>
         </div>
 
-    );
+    );}
+        else{
+            return (
+            <div className={classes.wrapper}>
+
+            <div className={classes.info}>
+                <h3>Movie title: <span>spanning</span></h3>
+                <h3>Issued to: <span>spanning</span></h3>
+                <h3>Start: <span>12:00</span></h3>
+            </div>
+            <div style={{ borderLeft: "8px solid red" }}>
+                <p className={classes.writing}>USED</p>
+            </div>
+            <div className={classes.torn}>
+                <h3 style={{ color: "#ff4b2b" }}>Cinefra</h3>
+                <h2>Seat num:</h2>
+                <h2>2</h2>
+            </div>
+        </div>);
+        }
 }
